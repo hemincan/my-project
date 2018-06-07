14acd8461a2b73829767c01f993568307d481414
hemincan <1027871354@qq.com>
2018-06-07 11:16:23 +0800

    加了个布局，然而并没有什么用

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,146 @@
 <template>
-  <div id="app">
-     <Menu mode="horizontal" theme="primary" active-key="1">
-        <Menu-item key="1">
-            <Icon type="ios-paper"></Icon>
-            内容管理
-        </Menu-item>
-        <Menu-item key="2">
-            <Icon type="ios-people"></Icon>
-            用户管理
-        </Menu-item>
-        <Submenu key="3">
-            <template slot="title">
-                <Icon type="stats-bars"></Icon>
-                统计分析
-            </template>
-            <Menu-group title="使用">
-                <Menu-item key="3-1">新增和启动</Menu-item>
-                <Menu-item key="3-2">活跃分析</Menu-item>
-                <Menu-item key="3-3">时段分析</Menu-item>
-            </Menu-group>
-            <Menu-group title="留存">
-                <Menu-item key="3-4">用户留存</Menu-item>
-                <Menu-item key="3-5">流失用户</Menu-item>
-            </Menu-group>
-        </Submenu>
-        <Menu-item key="4">
-            <Icon type="settings"></Icon>
-            综合设置
-        </Menu-item>
-    </Menu>
-    <router-view/>
-  </div>
+    <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+        <Row type="flex">
+            <i-col :span="spanLeft" class="layout-menu-left">
+                <Menu  theme="dark" accordion >
+                    <div class="layout-logo-left">
+                        
+                    </div>
+
+                     <Submenu key="1">
+                        <template slot="title">
+                            <Icon type="ios-paper"></Icon>
+                            内容管理
+                        </template>
+                        <Menu-item key="1-1">文章管理</Menu-item>
+                        <Menu-item key="1-2">评论管理</Menu-item>
+                        <Menu-item key="1-3">举报管理</Menu-item>
+                    </Submenu>
+                    <Submenu key="2">
+                        <template slot="title">
+                            <Icon type="ios-people"></Icon>
+                            用户管理
+                        </template>
+                        <Menu-item key="2-1">新增用户</Menu-item>
+                        <Menu-item key="2-2">活跃用户</Menu-item>
+                    </Submenu>
+                    <Submenu key="3">
+                        <template slot="title">
+                            <Icon type="stats-bars"></Icon>
+                            统计分析
+                        </template>
+                        <Menu-group title="使用">
+                            <Menu-item key="3-1">新增和启动</Menu-item>
+                            <Menu-item key="3-2">活跃分析</Menu-item>
+                            <Menu-item key="3-3">时段分析</Menu-item>
+                        </Menu-group>
+                        <Menu-group title="留存">
+                            <Menu-item key="3-4">用户留存</Menu-item>
+                            <Menu-item key="3-5">流失用户</Menu-item>
+                        </Menu-group>
+                    </Submenu>
+
+                </Menu>
+            </i-col>
+            <i-col :span="spanRight">
+                <div class="layout-header">
+                    <i-button type="text" @click="toggleClick">
+                        <Icon type="navicon" size="32"></Icon>
+                    </i-button>
+                </div>
+                <div class="layout-breadcrumb">
+                    <Breadcrumb>
+                        <Breadcrumb-item href="#">首页</Breadcrumb-item>
+                        <Breadcrumb-item href="#">应用中心</Breadcrumb-item>
+                        <Breadcrumb-item>某应用</Breadcrumb-item>
+                    </Breadcrumb>
+                </div>
+                <div class="layout-content">
+                    <div class="layout-content-main">
+                        <router-view/>
+                    </div>
+                </div>
+                <div class="layout-copy">
+                    2011-2016 &copy; TalkingData
+                </div>
+            </i-col>
+        </Row>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'App'
-}
+    export default {
+        data () {
+            return {
+                spanLeft: 5,
+                spanRight: 19
+            }
+        },
+        computed: {
+            iconSize () {
+                return this.spanLeft === 5 ? 14 : 24;
+            }
+        },
+        methods: {
+            toggleClick () {
+                if (this.spanLeft === 5) {
+                    this.spanLeft = 2;
+                    this.spanRight = 22;
+                } else {
+                    this.spanLeft = 5;
+                    this.spanRight = 19;
+                }
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        height: 78vh;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
+        background: #464c5b;
+    }
+    .layout-header{
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+    }
+    .layout-ceiling-main a{
+        color: #9ba7b5;
+    }
+    .layout-hide-text .layout-text{
+        display: none;
+    }
+    .ivu-col{
+        transition: width .2s ease-in-out;
+    }
 </style>
